794831bb77772eb4d4599c6df81f66a0b9f5ae6e
rifat32 <rifatbilalphilips@gmail.com>
2023-04-25 16:59:30 +0600

    update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,8 +7,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "1066221981614-0hhh9a07mjhf64am4jnthho6lg61d017.apps.googleusercontent.com",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "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",
+      clientId: "1066221981614-0hhh9a07mjhf64am4jnthho6lg61d017.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
   ],
 };
